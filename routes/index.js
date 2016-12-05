6dbecbe44969110636a8b4046716855b7223429f
João Ferreira <jaferrreira88@gmail.com>
2016-12-05 16:26:25 +0000

    testing

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var Client = require('node-rest-client').Client;
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var client = new Client();
+ 
+// direct way 
+client.get("http://52.169.24.36/v1/scrap/pt/chaves", function (data, response) {
+    res.render('index',data);
+});
+
+
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
