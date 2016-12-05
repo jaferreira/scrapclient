aba9eedd77225fe55613f5d01ed0ea716873ab47
João Ferreira <jaferrreira88@gmail.com>
2016-12-05 19:42:15 +0000

    port

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(process.env.Port || 8080)
+app.listen(process.env.Port || 3001)
 module.exports = app;
