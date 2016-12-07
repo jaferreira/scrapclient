420b64d2da462bc78856d9cb320ab2c655396bcb
João Ferreira <jaferrreira88@gmail.com>
2016-12-07 17:16:04 +0000

    teams

--- routes/teams.js
+++ routes/teams.js
@@ -5,6 +5,18 @@ var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 var url = 'mongodb://localhost:27017/scrap';
 
+var getDetailGame = function (db,name, callback) {
+    var collection = db.collection('games');
+    collection.find().toArray(function (err, items) {
+        if (err) {
+            console.log('error');
+            reject(err);
+        } else {
+            console.log('db ' + items);
+            callback(items);
+        }
+    });
+}
 
 
 router.get('/livestats/:teams', function (req, res, next) {
